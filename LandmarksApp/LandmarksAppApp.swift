0f5aa018bec0c67d1ab10827906673e1c4a78cde
bausov <stas909@gmail.com>
2023-06-08 22:34:06 +0300

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Stanislav Bausov on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
